2c14338d3126de44c1e1b1dd02d7ef18d6651fb3
Dromind <dominik.andrzej.drozd@gmail.com>
2024-07-13 10:57:50 +0200

    refactor(index.ts): get req query data and change types

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -4,22 +4,28 @@ import getContainer from "../ioc/inversify.config";
 import {COMMON_TYPES} from "../ioc/commonTypes";
 import {Logger} from "../commonServices/Logger";
 import {ILogger} from "../commonServices/ILogger";
-import {IFunctionService} from "./services/IFunctionService";
+import {IFunctionService} from "./services/function/IFunctionService";
+import {HttpResponse, ProcessedMessage} from "./types/apiTypes";
 
-const httpTrigger: AzureFunction = async (ctx: Context, req: HttpRequest): Promise<any> => {
+const httpTrigger: AzureFunction = async (ctx: Context, req: HttpRequest): Promise<HttpResponse> => {
     const container: Container = getContainer();
     const logger: Logger = container.get<ILogger>(COMMON_TYPES.ILogger) as Logger;
     logger.init(ctx, "1");
 
     const functionService: IFunctionService<any> =
         container.get<IFunctionService<any>>(COMMON_TYPES.IFunctionService);
-    const response: any = await functionService.processMessageAsync(req.body);
+
+    const ids: string[] = req.query.id ? req.query.id.split(',') : [];
+    const type: string = req.query.type;
+
+    const response: ProcessedMessage = await functionService.processMessageAsync({ ids, type }) as ProcessedMessage;
+
     ctx.res = {
         body: response,
         status: 200,
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
     };
-    return ctx.res;
+    return ctx.res as HttpResponse;
 };
 
 export default httpTrigger;
